ed0f22b7b5efd9992e2dac1ef3a169d93d20585f
Sectrumsun <taiwo.sunday@outlook.com>
2017-11-21 22:30:16 +0100

    added controllers'

---
+++ server/src/controllers/events/index.js
@@ -0,0 +1,9 @@
+import CreateEvent from './createEvent';
+import UpdateEvent from './updateEvent';
+import DeleteEvent from './deleteEvent'
+
+export{
+    CreateEvent,
+    UpdateEvent,
+    DeleteEvent
+}

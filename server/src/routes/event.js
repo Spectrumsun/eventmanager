5a58831b8e64b863018651227a28baaa7f3623b2
Sectrumsun <taiwo.sunday@outlook.com>
2017-11-21 22:29:15 +0100

    added routes files'

---
+++ server/src/routes/event.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import { CreateEvent, UpdateEvent, DeleteEvent } from '../controllers/events'
+
+const eventRouter = express.Router();
+
+eventRouter.post('/', CreateEvent.create)
+eventRouter.delete('/:id', DeleteEvent.delete)
+eventRouter.put('/:id', UpdateEvent.update)
+
+export default eventRouter;
---
+++ server/src/routes/index.js
@@ -0,0 +1,8 @@
+import centerRoute from './center';
+import eventRouter from './event'
+
+export{
+    centerRoute,
+    eventRouter
+    
+}

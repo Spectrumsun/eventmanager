a5afab58a5fa3069c520953a0fa7f50c7220fc07
Sectrumsun <taiwo.sunday@outlook.com>
2017-11-22 21:50:04 +0100

    apiEndpoint

---
+++ server/src/controllers/center/addCenter.js
@@ -0,0 +1,20 @@
+import dbc from '../../data/dbc.json'
+
+const data = dbc.center
+
+class AddCenter {
+  static add(req, res) {
+    data.push({
+        id: req.body.id,
+        name: req.body.name,
+        city: req.body.city,
+        addres: req.body.address,
+        facility: req.body.facility
+    })
+    res.status(200).send({
+        message: 'success',
+        data
+        })
+    }
+}
+export default AddCenter
---
+++ server/src/routes/center.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import { AllCenters, UpdateCenter, FindCenter, AddCenter } from '../controllers/center';
+
+const centerRoute = express.Router();
+
+centerRoute.get('/', AllCenters.all);
+centerRoute.post('/', AddCenter.add);
+centerRoute.get('/:id', FindCenter.find);
+centerRoute.put('/:id', UpdateCenter.update);
+
+export default centerRoute;

54a10911c26d6842d1ee117b03366c606c820274
Sectrumsun <taiwo.sunday@outlook.com>
2017-11-27 10:22:19 +0100

    add_Get_Logic_to _get_all_centers_in_the_DB

--- server/controllers/centerController.js
+++ server/controllers/centerController.js
@@ -1,63 +1,11 @@
-import dbc from './../data/dbc.json'
+import db from '../models'
 
-const data = dbc.center
-
-
-  
-
-exports.allCenters = (req, res) => {
-    res.status(200).send(data);
-  }
+const centerDB = db.Center
 
 
-exports.addCenter = (req, res) => {
-    data.push({
-      id: req.body.id,
-      name: req.body.name,
-      city: req.body.city,
-      address: req.body.address,
-      facility: req.body.facility
-    })
-    res.status(201).send({
-      message: 'success',
-      data
-    })
-  }
-
-
-
-exports.findCenter = (req, res) => {
-    const { id } = req.params
-    for (let i = 0; i < data.length; i++) {
-      if (id === data[i].id) {
-        return res.status(302).json({
-          message: 'found',
-          id: data[i]
-        })
-      }
-    }
-    res.status(404).json({
-      message: 'center doesnt exist!'
-    })
-  }
-
-
-
-exports.updateCenter = (req, res) => {
-    const { id } = req.params
-    for (let i = 0; i < data.length; i++) {
-      if (id === data[i].id) {
-        data[i].name = req.body.name
-        data[i].city = req.body.city
-        data[i].address = req.body.address
-        data[i].facility = req.body.facility
-        return res.status(200).json({
-          message: 'edited',
-          id: data[i]
-        })
-      }
-    }
-    res.status(404).json({
-      message: 'event doesnt exist!'
-    })
+class Center {
+  static getCenter(req, res) {
+    centerDB.all()
+      .then(center => res.status(200).send({ message: 'success', center }))
+      .catch(error => res.status(200).send(error))
   }

c4956fd7ef0379765bbc8ab0a2dc4078db87b0de
Sectrumsun <taiwo.sunday@outlook.com>
2017-11-27 07:35:45 +0100

    restructure file and folder

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,29 @@
-import centerRoute from './center'
-import eventRouter from './event'
+import express from 'express'
+import eventController from '../controllers/eventController'
+import centerController from '../controllers/centerController'
+import userController from '../controllers/userController'
 
-export {
-  centerRoute,
-  eventRouter
 
-}
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  res.status(200).send({ message: 'welcome to our Event Manager' })
+})
+
+
+router.get('/events', eventController.getEvent)
+router.get('/events/:id', eventController.getOneEvent)
+router.post('/events', eventController.createEvent)
+router.put('/events/:id', eventController.editEvent)
+router.delete('/events/:id', eventController.deleteEvent)
+
+
+router.get('/centers', centerController.getCenter)
+router.get('/centers/:id', centerController.getOneCenter)
+router.post('/centers', centerController.createCenter)
+router.put('/centers/:id', centerController.editCenter)
+router.delete('/centers/:id', centerController.deleteCenter)
+
+
+
+export default router

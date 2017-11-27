b2222d537281265858dbd5937b08c6f8228ee580
Sectrumsun <taiwo.sunday@outlook.com>
2017-11-27 11:58:29 +0100

    'fix_lint_errors

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,36 +4,33 @@ import bcrpt from 'bcrypt'
 
 const userDB = db.User
 
-//require('dotenv').config()
+// require('dotenv').config()
 
-const Secret = process.env.SECRET;
+const Secret = process.env.SECRET
 
 class User {
-
-
-
-
-  static login (req, res) {
+  static login(req, res) {
     userDB.findOne({
       where: {
         email: req.body.Email
       },
     })
       .then((user) => {
-        if(user) {
+        if (user) {
           bcrypt.compare(req.body.password, user.password, (err, response) => {
-            if(response) {
+            if (response) {
               const token = jwt.sign({
                 id: user.id,
                 fullname: user.fullname,
                 email: user.email
-              }, Secret, { expiresIn: '24h' });
+              }, Secret, { expiresIn: '24h' })
               return res.status(200).send({
                 message: `Welcome ${user.email} `,
                 fullname: user.fullname,
-                token});
-              }
-              return res.status(409).send({ message: 'email or password incorrect'})
+                token
+              })
+            }
+            return res.status(409).send({ message: 'email or password incorrect' })
           })
         } else {
           res.status(404).send({
@@ -43,7 +40,6 @@ class User {
       })
   }
 }
-
 
 
 export default User

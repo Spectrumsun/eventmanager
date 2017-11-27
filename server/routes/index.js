d8d99703815e79e853a70cb01fa551017c3f1f9e
Sectrumsun <taiwo.sunday@outlook.com>
2017-11-27 12:20:30 +0100

    fix_server_file

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,32 +1,31 @@
-import express from 'express';
-import eventController from '../controllers/eventController';
-import centerController from '../controllers/centerController';
-import userController from '../controllers/userController';
+import express from 'express'
+import eventController from '../controllers/eventController'
+import centerController from '../controllers/centerController'
+import userController from '../controllers/userController'
 
-
-const router = express.Router();
-
+const router = express.Router()
 
 /* Get Home Page. */
 
 router.get('/', (req, res) => {
-  res.status(200).send({ message: 'welcome to our Event Manager' });
-});
+  res
+    .status(200)
+    .send({message: 'welcome to our Event Manager'})
+})
 
+router.get('/events', eventController.getEvent)
+router.get('/events/:id', eventController.getOneEvent)
+router.post('/events', eventController.createEvent)
+router.put('/events/:id', eventController.editEvent)
+router.delete('/events/:id', eventController.deleteEvent)
 
-router.get('/events', eventController.getEvent);
-router.get('/events/:id', eventController.getOneEvent);
-router.post('/events', eventController.createEvent);
-router.put('/events/:id', eventController.editEvent);
-router.delete('/events/:id', eventController.deleteEvent);
+router.get('/centers', centerController.getCenter)
+router.get('/centers/:id', centerController.getOneCenter)
+router.post('/centers', centerController.createCenter)
+router.put('/centers/:id', centerController.editCenter)
+router.delete('/centers/:id', centerController.deleteCenter)
 
+router.post('/users/signup', userController.signup)
+router.post('/users/login', userController.login)
 
-router.get('/centers', centerController.getCenter);
-router.get('/centers/:id', centerController.getOneCenter);
-router.post('/centers', centerController.createCenter);
-router.put('/centers/:id', centerController.editCenter);
-router.delete('/centers/:id', centerController.deleteCenter);
-
-
-router.post('/users/signup', userController.signup);
-
+export default router

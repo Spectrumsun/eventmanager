f7e7cb5ac00ad80c50b6d6b92dd325b7c4fc80ed
Sectrumsun <taiwo.sunday@outlook.com>
2017-12-02 12:59:40 +0100

    add date validation

--- server/middleware/validator.js
+++ server/middleware/validator.js
@@ -1,7 +1,6 @@
 import db from '../models';
+import { Center, Event } from '../models';
 
-const Center = db.Center;
-const Event = db.Event;
 
 class Validate {
   static validateSigup(req, res, next) {
@@ -10,7 +9,6 @@ class Validate {
     req.checkBody('email', 'That Email is not valid!').isEmail();
     req.sanitizeBody('email').normalizeEmail({ remove_dots: false, remove_extension: false, gmail_remove_subaddress: false });
     req.checkBody('password', 'Password Cannot be Blank!').notEmpty();
-    req.checkBody('confirmPassword', 'Confirmed Password cannot be blank!').notEmpty();
     req.checkBody('confirmPassword', 'Oops! Your passwords do not match').equals(req.body.password);
 
     const errors = req.validationErrors();
@@ -58,14 +56,28 @@ class Validate {
     const errors = req.validationErrors();
     if (errors) {
       return res.status(400).send({ message: 'Error adding new Center', errors });
-      // stop the fn from running
     }
     next();
   }
 
   static checkDate(req, res, next) {
+    if ((new Date(req.body.date) - Date.now()) < 0) {
+  	     return res.status(400).send({
+        message: 'You Can not set a past date for an  event'
+      });
+    }
+
+    if ((new Date(req.body.date) + Date.now()) > 0) {
+  	     return res.status(400).send({
+        message: 'You Can not set a past date for an  event'
+      });
+    }
+
+
     const newCenter = req.body.date;
     const userInfo = req.body.id;
+
+
     Center.findById(req.body.center, { include: [{ model: Event, as: 'events', }], })
       .then((center) => {
         const centerItems = center.toJSON();
@@ -81,7 +93,10 @@ class Validate {
         const newDate = dates;
         for (let i = 0; i < newDate.length; i++) {
           if (newCenter == newDate[i]) {
-            return res.status(400).send({ message: `Sorry Center booked for that date. Please look through the aleady booked dates for the centers ${dates}. You can choose another date or another center.` });
+            return res.status(400).send({
+              message: `Sorry Center booked for that date. Please look through the aleady booked dates for the centers ${dates}.
+            You can choose another date or another center.`
+            });
           }
         }
         next();

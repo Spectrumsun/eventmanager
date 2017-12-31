bacb0a98ec69b91b30f48dc12bb26b55b2fd0fed
Sectrumsun <taiwo.sunday@outlook.com>
2017-12-31 16:21:18 +0100

    add center api

--- client/src/components/ViewCenters.js
+++ client/src/components/ViewCenters.js
@@ -2,9 +2,19 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class Centers extends Component {
-  state = {
+  /*    state = {
+    id: '',
+    centerName: '',
+    address: '',
+  } */
 
+  componentDidMount() {
+    axios.get('/centers')
+      .then((res) => {
+        console.log(res);
+      });
   }
+
   render() {
     return (
       <div className="container" style={{ paddingTop: '100px' }}>
@@ -13,27 +23,6 @@ class Centers extends Component {
           <div className="card-body">
             <h4 className="card-title">Havilah Event Centre</h4>
             <h6 className="card-subtitle mb-2 text-muted">No 33 Ignobi Street Lagos</h6>
-            <a href="centerinfo.html" className="card-link">View More</a>
-          </div>
-        </div>
-        <div className="card d-lg-inline-block" style={{ width: '20rem' }}>
-          <div className="card-body">
-            <h4 className="card-title">Nikoms Event Center </h4>
-            <h6 className="card-subtitle mb-2 text-muted">CNo 12 Yaba road Surulere Lagos</h6>
-            <a href="centerinfo.html" className="card-link">View More</a>
-          </div>
-        </div>
-        <div className="card d-lg-inline-block" style={{ width: '20rem' }}>
-          <div className="card-body">
-            <h4 className="card-title">Panda Event Centre</h4>
-            <h6 className="card-subtitle mb-2 text-muted">No 12 Oyediran Street Lagos</h6>
-            <a href="centerinfo.html" className="card-link">View More</a>
-          </div>
-        </div>
-        <div className="card d-lg-inline-block" style={{ width: '20rem' }}>
-          <div className="card-body">
-            <h4 className="card-title">Yaba Center</h4>
-            <h6 className="card-subtitle mb-2 text-muted">No 12 Yaba road Surulere Lagos</h6>
             <a href="centerinfo.html" className="card-link">View More</a>
           </div>
         </div>

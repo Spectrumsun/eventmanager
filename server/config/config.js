d007fd95efc81cf68fac60d34160933fa81ebb52
Taiwo Sunday <taiwosunday@Andelas-MacBook-Pro-91.local>
2018-04-06 21:27:28 +0100

    fix package json

--- server/config/config.js
+++ server/config/config.js
@@ -6,15 +6,15 @@ module.exports = {
     password: process.env.configPassword,
     database: process.env.configDatabase,
     host: process.env.configHost,
-    dialect: process.env.configDialect || 'postgres',
+    dialect: process.env.configDialect,
     port: 5432,
   },
   test: {
     url: process.env.DATABASE_URL_TEST,
-    dialect: process.env.configDialect || 'postgres'
+    dialect: process.env.configDialect
   },
   production: {
-    url: process.env.configEnvVar,
-    dialect: process.env.configDialect || 'postgres'
+    url: 'postgres://cegrrihw:rnQj8f_xe0Ariy3OdYv6sbNX5MxoXcK_@baasu.db.elephantsql.com:5432/cegrrihw',
+    dialect: process.env.configDialect
   }
 };
